df3f698dacedb22d1e82fb59c7e519401c2e888b
InnaVit <innavit007@gmail.com>
2023-03-06 21:17:46 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryContainer = document.querySelector('.gallery');
-const card = (galleryItemCreate(galleryItems));
+const galleryContainer = document.querySelector(".gallery");
+const card = galleryItemCreate(galleryItems);
 console.log(card);
 
 galleryContainer.insertAdjacentHTML("beforeend", card);
 console.log(galleryContainer);
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 
-function galleryItemCreate (galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
-          return `<div class="gallery__item">
+function galleryItemCreate(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
                <a class="gallery__link" href="${original}">
                 <img
                     class="gallery__image"
@@ -21,33 +22,30 @@ function galleryItemCreate (galleryItems) {
                     alt="${description}"
                 />
                 </a>
-                </div>`;})
-    .join('');
-    
+                </div>`;
+    })
+    .join("");
 }
 
 function onGalleryContainerClick(e) {
-    e.preventDefault();
-    
-    if (e.target.nodeName !== "IMG") {
-        return;
-    }
+  e.preventDefault();
 
-    const bigImageUrl = e.target.dataset.source;
-    console.log(bigImageUrl);
-    const instance = basicLightbox.create(`
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const bigImageUrl = e.target.dataset.source;
+  console.log(bigImageUrl);
+  const instance = basicLightbox.create(`
 		<img width="1400" height="900" src="${bigImageUrl}">
 	`);
-    instance.show();
+  instance.show();
 
-    window.addEventListener('keydown', (e) => {
-        if (e.code === "Escape") {
-            instance.close();
-        }
-    })
+  window.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
 }
 
 console.log(galleryItems);
-
-
-
